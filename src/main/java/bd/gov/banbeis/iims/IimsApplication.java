2336c79f964c63fa349f8fb3b2afa0f11c05f6b0
monjurmorshed793 <monjurmorshed793@gmail.com>
2022-08-01 23:07:13 +0600

    initial IIMS project set up

---
+++ src/main/java/bd/gov/banbeis/iims/IimsApplication.java
@@ -0,0 +1,13 @@
+package bd.gov.banbeis.iims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IimsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bd/gov/banbeis/iims/IimsApplicationTests.java
@@ -0,0 +1,13 @@
+package bd.gov.banbeis.iims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
